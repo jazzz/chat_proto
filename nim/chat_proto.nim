1f6c75bf6ab6d218e9bcffb7eebdc155d955bd7e
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2025-07-03 23:19:48 -0700

    update nim types

--- nim/chat_proto.nim
+++ nim/chat_proto.nim
@@ -1,7 +1,9 @@
 import protobuf_serialization
 import protobuf_serialization/proto_parser
 
-import_proto3 "./proto/base.proto"
+
+import_proto3 "./proto/envelope.proto"
+import_proto3 "./proto/reliability.proto"
 
 export protobuf_serialization
 
